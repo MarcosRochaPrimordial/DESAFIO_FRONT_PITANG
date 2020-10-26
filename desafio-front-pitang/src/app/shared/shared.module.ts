5d0e773464fc4a6c4724237a262addce8c24a6ce
MarcosRochaPrimordial <markossdeath@gmail.com>
2020-10-26 17:15:32 +0000

    removing footer

--- desafio-front-pitang/src/app/shared/shared.module.ts
+++ desafio-front-pitang/src/app/shared/shared.module.ts
@@ -6,7 +6,6 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { FooterComponent } from './components/footer/footer.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatTabsModule } from '@angular/material/tabs';
@@ -19,7 +18,6 @@ import { RouterModule } from '@angular/router';
 
 const components = [
   HeaderComponent,
-  FooterComponent,
   MessageComponent,
 ];
 
